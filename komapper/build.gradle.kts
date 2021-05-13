edb727f7ff40a44727c5898079dd6b6f4ed22680
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-05-13 18:57:39 +0900

    Update com.google.devtools.ksp and komapper

--- komapper/build.gradle.kts
+++ komapper/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     idea
     application
     kotlin("jvm")
-    id("com.google.devtools.ksp") version "1.5.0-1.0.0-alpha09"
+    id("com.google.devtools.ksp") version "1.5.0-1.0.0-alpha10"
 }
 
 java {
@@ -19,8 +19,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.komapper:komapper-starter:0.6.1")
-    ksp("org.komapper:komapper-processor:0.6.1")
+    implementation("org.komapper:komapper-starter:0.7.0")
+    ksp("org.komapper:komapper-processor:0.7.0")
 }
 
 tasks.withType<KotlinCompile>() {
